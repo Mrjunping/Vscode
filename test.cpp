dc5009066418e3763ecbf15fbbebcc0a6a7daadf
Mrjunping <378442051@qq.com>
2022-08-13 15:01:00 +0800

    xxj go

--- test.cpp
+++ test.cpp
@@ -2,9 +2,5 @@
 
 using namespace std;
 int main(){
-    cout << "hello world!" <<endl;   
-    cout << "hello test!" <<endl;
-    cout << "hhhhh" <<endl;
-    cout << "yjp" << endl;
-    cout << "zoulu" << endl;
+    cout << "xxj zoulu " << endl;
 }
